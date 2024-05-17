64f0424150a51b99d9435a60079863cfc8ccc6e0
fareedfotuhi <farfotfir@gmail.com>
2024-05-17 13:25:02 +0330

    add test

---
+++ OOD_Patterns/src/test.java
@@ -0,0 +1,31 @@
+package test;
+
+import Package.java;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class test {
+    @Test
+    void changeShippingStrategy_test() {
+        Pachage Package = new Package();
+    }
+
+    @Test
+    void updatePackageState_test() {
+        Pachage Package = new Package();
+    }
+
+    @Test
+    void onDeliver_test() {
+        Pachage Package = new Package();
+    }
+
+    @Test
+    void printInfo_test() {
+        Pachage Package = new Package();
+    }
+
+
+
+}
